254708bc6d35d037daed91853d98aedece3ea844
renxing <ren2436@126.com>
2021-09-02 22:36:55 +0800

    add

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app-container">
+    <div class="header">
+      <nuxt-link to="/">首页</nuxt-link> |
+      <nuxt-link to="/about">我的</nuxt-link>
+    </div>
+
+    <div class="app-main">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+<script>
+export default {}
+</script>
